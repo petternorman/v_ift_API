faf24f9b399af9c0937b03f60e4462f63cdff62a
Petter Norman <petter.norman@valtech.se>
2014-11-23 16:16:03 +0100

    add get lobby

---
+++ v_ift/NancyModules/Status.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Nancy;
+using Nancy.ModelBinding;
+using v_ift.Classes.Repositories;
+using v_ift.Models;
+using v_ift.ResponseModels;
+
+namespace v_ift.NancyModules
+{
+	public class Status : NancyModule
+	{
+		public Status(IRepository repository)
+		{
+			Get["/getlobby", true] = async (x, ct) =>
+			{
+				var request = this.Bind<LobbyModel>();
+
+				if (request == null)
+				{
+					return null;
+				}
+
+				var lobby = repository.GetLobby(request.LobbyId);
+
+				if (lobby == null)
+				{
+					return new Response {StatusCode = HttpStatusCode.NotFound};
+				}
+
+				return Response.AsJson(new Lobby(lobby));
+			};
+		}
+	}
+}
