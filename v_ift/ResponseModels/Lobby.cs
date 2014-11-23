7b529e7a26645157748a26383ca70285bbcf4a42
Sandra Nystrom <sandra.nystrom@valtech.se>
2014-11-23 12:11:02 +0100

    lagt till status

--- v_ift/ResponseModels/Lobby.cs
+++ v_ift/ResponseModels/Lobby.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using v_ift.Models;
 
 namespace v_ift.ResponseModels
 {
     public class Lobby
     {
+        [JsonConverter(typeof(StringEnumConverter))]
         public Enums.Status Status { get; set; }
 
         public List<Player> Players { get; set; }
