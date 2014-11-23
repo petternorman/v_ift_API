f96c2672d060f5c9914d386363e28ffcce74f034
emra <emma.rangert@gmail.com>
2014-11-23 12:35:35 +0100

    updated join

--- v_ift/NancyModules/Join.cs
+++ v_ift/NancyModules/Join.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using Nancy;
+using Nancy.ModelBinding;
+using v_ift.Models;
+using v_ift.ResponseModels;
 
 namespace v_ift.NancyModules
 {
@@ -10,10 +14,25 @@ namespace v_ift.NancyModules
 
             Post["/join/{lobby_id}", true] = async (x, ct) =>
             {
-                var join = new ResponseModels.Player();
-                join.Name = "";
-                join.Guid = new Guid();
-                return Response.AsJson(join);
+                var join = this.Bind<JoinModel>();
+
+                var player = new ResponseModels.Player()
+                {
+                    Name = join.Name,
+                    Guid = new Guid()
+                };
+
+                var lobbyGuid = new Guid();
+                var players = new List<Player>();
+
+                var status = new Lobby()
+                {
+                    Status = Enums.Status.Waiting,
+                    LobbyGuid = lobbyGuid,
+                    Players = players
+                };
+
+                return Response.AsJson(status);
                 //return new Response {StatusCode = HttpStatusCode.OK};
             };
         }
