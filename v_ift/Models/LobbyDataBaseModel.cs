28ab8d9c69356fa389201ae7de7b23212922af1a
Sandra Nystrom <sandra.nystrom@valtech.se>
2014-11-23 14:35:22 +0100

    add files

---
+++ v_ift/Models/LobbyDataBaseModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using v_ift.ResponseModels;
+
+namespace v_ift.Models
+{
+    public class LobbyDataBaseModel
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Enums.Status Status { get; set; }
+
+        public List<PlayerDataModel> Players { get; set; }
+
+        public string LobbyName { get; set; }
+
+        public int Count { get; set; }
+
+        public ObjectId _id { get; set; }
+    }
+}
---
+++ v_ift/NancyModules/CreateLobby.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.ModelBinding;
+using v_ift.Classes.Repositories;
+using v_ift.Models;
+using v_ift.ResponseModels;
+
+namespace v_ift.NancyModules
+{
+    public class CreateLobby : NancyModule
+    {
+        public CreateLobby(IRepository repository)
+        {
+            Post["/createLobby", true] = async (x, ct) =>
+            {
+                var lobby = new LobbyDataBaseModel
+                {
+                    Status = Enums.Status.Waiting,
+                    LobbyName = "v_iftlobby",
+                    Players = new List<PlayerDataModel>()
+                };
+
+                repository.SaveLobby(lobby);
+
+                 return new Response { StatusCode = HttpStatusCode.NotFound };
+            };
+        }
+    }
+}
