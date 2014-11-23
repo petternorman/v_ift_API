d12d53657528f106f373d8cab6ab49fb815f9f65
Petter Norman <petter.norman@valtech.se>
2014-11-23 12:25:00 +0100

    get database with dpi

---
+++ v_ift/Classes/DatabaseHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using MongoDB.Driver;
+
+namespace v_ift.Classes
+{
+	public interface IDatabaseHelper
+	{
+		MongoDatabase GetMongoDatabase();
+	}
+
+	public class DatabaseHelper : IDatabaseHelper
+	{
+		public MongoDatabase GetMongoDatabase()
+		{
+			var connectionstring = ConfigurationManager.AppSettings.Get("(MONGOHQ_URL|MONGOLAB_URI)");
+			var url = new MongoUrl(connectionstring);
+			var client = new MongoClient(url);
+			var server = client.GetServer();
+			var database = server.GetDatabase(url.DatabaseName);
+			return database;
+		}
+	}
+}
