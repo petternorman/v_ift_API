e7640b36e99878383db82dd6f9d1a3a8d3db18dd
Sandra Nystrom <sandra.nystrom@valtech.se>
2014-11-23 10:35:35 +0100

    add files

---
+++ v_ift/NancyModules/Create.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+
+namespace v_ift.NancyModules
+{
+    public class Create : NancyModule
+    {
+        public Create()
+        {
+            Post["/join", true] = async (x, ct) =>
+            {
+
+                return new Response {StatusCode = HttpStatusCode.OK};
+            };
+        }
+    }
+}
