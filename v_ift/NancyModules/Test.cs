00eed15f5d14055c7d4053108cd86631987b0e32
Sandra Nystrom <sandra.nystrom@valtech.se>
2014-11-23 11:05:37 +0100

    fix nancy

--- v_ift/NancyModules/Test.cs
+++ v_ift/NancyModules/Test.cs
@@ -1,9 +1,15 @@
 ﻿using Nancy;
 
-namespace v_ift.NancyModuls
+namespace v_ift.NancyModules
 {
     public class Test : NancyModule
     {
-
+        public Test()
+        {
+            this.Get["/test/{id}/}"] = parameters =>
+            {
+                return new Response {StatusCode = HttpStatusCode.OK};
+            };
+        }
     }
 }
