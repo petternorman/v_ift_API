3184eaf955d252c21529951101495684499be7ff
Sandra Nystrom <sandra.nystrom@valtech.se>
2014-11-23 11:47:22 +0100

    add ready

---
+++ v_ift/Models/ReadyModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace v_ift.Models
+{
+    public class ReadyModel
+    {
+        public string LobbyId { get; set; }
+
+        public string PlayerId { get; set; }
+    }
+}
---
+++ v_ift/NancyModules/Ready.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.ModelBinding;
+using v_ift.Models;
+
+namespace v_ift.NancyModules
+{
+    public class Ready : NancyModule
+    {
+        public Ready()
+        {
+            Post["/ready", true] = async (x, ct) =>
+            {
+                var ready = this.Bind<ReadyModel>();
+
+                return new Response { StatusCode = HttpStatusCode.OK };
+            };
+        }
+    }
+}
