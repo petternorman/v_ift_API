482936fb466d488e7b6054f725254136ce3de6af
Petter Norman <petter.norman@valtech.se>
2014-11-23 10:44:07 +0100

    added mongodb driver

--- v_ift/NancyModules/Test.cs
+++ v_ift/NancyModules/Test.cs
@@ -1,9 +1,23 @@
-﻿using Nancy;
+﻿using System.Configuration;
+using MongoDB.Driver;
+using Nancy;
 
 namespace v_ift.NancyModuls
 {
-    public class Test : NancyModule
-    {
+	public class Test : NancyModule
+	{
+		public MongoDatabase Database
+		{
+			get
+			{
+				var connectionstring = ConfigurationManager.AppSettings.Get("(MONGOHQ_URL|MONGOLAB_URI)");
+				var url = new MongoUrl(connectionstring);
+				var client = new MongoClient(url);
+				var server = client.GetServer();
+				var database = server.GetDatabase(url.DatabaseName);
+				return database;
+			}
+		}
+	}
 
-    }
 }
